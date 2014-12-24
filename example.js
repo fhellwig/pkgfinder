48247f24ee23cb9f52451b75ef9c08c4497db96d
Frank Hellwig <frank.hellwig@buchanan-edwards.com>
2014-12-23 23:37:22 -0500

    Removed example.js

--- example.js
+++
@@ -1,5 +0,0 @@
-var pkgfinder = require('./pkgfinder');
-console.log('name: %s', pkgfinder.name);
-console.log('directory: %s', pkgfinder.directory);
-console.log('resolve: %s', pkgfinder.resolve('data'));
-console.log('isCurrent: %s', pkgfinder.isCurrent());
